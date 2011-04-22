32c6852cb33e932488e2f848c71f6c6bb71aee2a
whtt-eric <eric@wehaventthetime.com>
2011-04-22 16:37:47 -0400

    adjustment to git recipe to add to .gitignore

--- lib/beet/recipes/rails3/git.rb
+++ lib/beet/recipes/rails3/git.rb
@@ -1,8 +1,16 @@
-# .gitignore is now created for us by rails
-# only need to init git, add the current dir, and commit!
+remove_file '.gitignore'
+file '.gitignore', <<-CODE.gsub(/^ {2}/, '')
+  .DS_Store
+  .bundle
+  .rvmrc
+  mkmf.log
+  log/*
+  coverage/*
+  tmp/**/*
+  config/database.yml
+  coverage/*
+CODE
 
 git :init
-
 git :add => "."
-
 git :commit => "-m 'Initial commit from Beet. Enjoy.'"
