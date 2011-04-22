c9966a6107dcb355a57f38ffd5cf6d596ca563e0
whtt-eric <eric@wehaventthetime.com>
2011-04-22 16:39:30 -0400

    change mysql recipe to use mysql2 gem

--- lib/beet/recipes/rails3/db/mysql.rb
+++ lib/beet/recipes/rails3/db/mysql.rb
@@ -1,26 +1,26 @@
-gem "mysql"
+gem "mysql2"
 
 run "bundle install"
 
 file 'config/database.yml' do
-  %{  
+  %{
 ---
 development: &defaults
-  adapter: mysql
+  adapter: mysql2
   encoding: utf8
   reconnect: false
   database: #{project_name}_development
   pool: 5
   username: root
   password:
- 
+
 # Warning: The database defined as "test" will be erased and
 # re-generated from your development database when you run "rake".
 # Do not set this db to the same as development or production.
 test:
   <<:       *defaults
   database: #{project_name}_test
- 
+
 production:
   <<:       *defaults
   database: #{project_name}_production
