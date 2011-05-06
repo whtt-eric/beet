7f8cc3d3ddb692ded3fda196c7a8292b01b67d36
whtt-eric <eric@wehaventthetime.com>
2011-05-06 09:10:02 -0400

    update to rspec recipe - include additional testing gems, added config code for mocha, factory_girl and email_spec

--- lib/beet/recipes/rails3/testing/rspec.rb
+++ lib/beet/recipes/rails3/testing/rspec.rb
@@ -1,6 +1,12 @@
-gem 'rspec-rails', :version => '~> 2.0.0.beta.19', :group => [:development,:test]
+gem_group [:development, :test] do
+  gem 'rspec-rails', :version => '2.5.0'
+  gem 'shoulda-matchers', :version => '1.0.0.beta2'
+  gem 'mocha', :version => '0.9.12'
+  gem 'factory_girl_rails', :version => '1.0.1'
+  gem 'email_spec', :version => '1.1.1'
+end
 
-run "bundle install"
+rvm "bundle install"
 
 generate "rspec:install"
 
@@ -10,6 +16,25 @@ in_root do
     config.generators do |g|
       g.test_framework :rspec
     end
+}
+  end
+
+  # mocha config
+  gsub_file "spec/spec_helper.rb", /(config.mock_with :rspec)/, '# \1'
+  gsub_file "spec/spec_helper.rb", /#\s*(config.mock_with :mocha)/, '\1'
+
+  # factory_girl_rails config
+  gsub_file "spec/spec_helper.rb", /(config.fixture_path = "\#\{::Rails.root\}\/spec\/fixtures")/, '# \1'
+
+  # email_spec config
+  add_after "spec/spec_helper.rb", "require 'rspec/rails'" do
+    %{require "email_spec"}
+  end
+
+  add_after "spec/spec_helper.rb", "RSpec.configure do |config|" do
+%{
+  config.include(EmailSpec::Helpers)
+  config.include(EmailSpec::Matchers)
 }
   end
 end
