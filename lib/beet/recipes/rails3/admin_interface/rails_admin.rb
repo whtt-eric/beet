0ba036eff16708f6b130d05cbfa4c54973a9d229
whtt-eric <eric@wehaventthetime.com>
2011-05-06 10:18:57 -0400

    updated rails_admin recipe - use rvm; ask for Devise model name

--- lib/beet/recipes/rails3/admin_interface/rails_admin.rb
+++ lib/beet/recipes/rails3/admin_interface/rails_admin.rb
@@ -1,6 +1,9 @@
 gem 'devise'
 gem 'rails_admin', :git => 'git://github.com/sferik/rails_admin.git'
 
-run "bundle update"
+rvm "bundle update"
 
-generate "rails_admin:install_admin"
+model_name = ask "What is the name of your Devise model? (e.g. User, Admin)"
+model_name = "User" if model_name.empty?
+
+generate "rails_admin:install_admin model_name=#{model_name}"
