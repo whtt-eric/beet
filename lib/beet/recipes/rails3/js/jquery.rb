5f7ef3bb086d418705e7167ff601a1e28c9fadc9
whtt-eric <eric@wehaventthetime.com>
2011-05-06 10:05:24 -0400

    updated jquery recipe - use rvm for bundle install; ask to include jquery ui; ask for version to use

--- lib/beet/recipes/rails3/js/jquery.rb
+++ lib/beet/recipes/rails3/js/jquery.rb
@@ -1,5 +1,12 @@
-gem "jquery-rails"
+gem "jquery-rails", :version => '1.0'
 
-run "bundle install"
+rvm "bundle install"
 
-generate "jquery:install --ui"
+command = "jquery:install"
+command += " --ui" if yes?("Include jQuery UI?")
+
+version = ask "Version of jQuery to install? (default is 1.6)"
+version = "1.6" if version.empty?
+command += " --version #{version}"
+
+generate command
