63f1b561da659e6ffa8068b4ab1eb17932fefa49
whtt-eric <eric@wehaventthetime.com>
2011-05-06 09:46:54 -0400

    Update to devise recipe - updated version, ask for Devise model name, ask to generate views and run migration

--- lib/beet/recipes/rails3/auth/devise.rb
+++ lib/beet/recipes/rails3/auth/devise.rb
@@ -1,8 +1,17 @@
 # this is designed for rails 3
-gem 'devise', :version => '1.1.2'
+gem 'devise', :version => '1.3.4'
 
 generate "devise:install"
-generate "devise User"
-generate "devise:views"
 
-rake "db:migrate"
+model_name = ask "What would you like to call your Devise model? (e.g. User, Admin)"
+model_name = "User" if model_name.empty?
+
+generate "devise #{model_name}"
+
+if yes?("Generate Devise views?")
+  generate "devise:views"
+end
+
+if yes?("Run migration to create #{model_name} table?")
+  rake "db:migrate"
+end
